3ca7e6cea13c5206cd35481aeeb6c8621b3006fc
hafidzfawwaz6 <mhafidzfawwaz@gmail.com>
2024-08-12 22:50:49 +0700

    updated app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import joblib
 import pandas as pd
 from flask import Flask, jsonify, request
+import re
 
 model = joblib.load('book_recommender_model.joblib')
 
@@ -15,12 +16,18 @@ app = Flask(__name__)
 def get_index():
     return jsonify(df.index.tolist())
 
+def normalize_title(title):
+    """Normalize the book title by stripping spaces, lowercasing, and removing special characters."""
+    return re.sub(r'\W+', '', str(title).strip().lower())
+
 @app.route('/recommend', methods=['GET'])
 def recommend():
     title = request.args.get('title').strip().lower().replace("'", '')
 
+    normalized_title = normalize_title(title)
+
     try:
-        matching_index = df.index.str.strip().str.lower().str.replace("'", '').get_loc(title)
+        matching_index = df.index.to_series().apply(normalize_title).get_loc(normalized_title)
     except KeyError:
         return jsonify({'error': f"Book '{title}' not found"}), 404
 
@@ -35,17 +42,24 @@ def recommend():
 
         books_info = []
         for rec_title in recommended_titles:
-            book_info = books_df.loc[rec_title]
-            books_info.append({
-                'title': rec_title,
-                'author': book_info['Book-Author'],
-                'year': book_info['Year-Of-Publication'],
-                'publisher': book_info['Publisher']
-            })
+            normalized_rec_title = normalize_title(rec_title)
+
+            try:
+                book_info = books_df.loc[books_df.index.to_series().apply(normalize_title) == normalized_rec_title]
+                book_info = book_info.iloc[0]  
+                books_info.append({
+                    'title': rec_title,
+                    'author': book_info['Book-Author'],
+                    'year': book_info['Year-Of-Publication'],
+                    'publisher': book_info['Publisher']
+                })
+            except IndexError:
+                print(f"Book '{rec_title}' not found in the books DataFrame.")
 
         return jsonify({'recommended_titles': recommended_titles, 'books_info': books_info})
     else:
         return jsonify({'error': f"Book '{title}' not found"}), 404
+
 
 
 if __name__ == '__main__':
