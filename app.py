c8fc89f42528fdc2b01c6ee1e550d8b47adeca25
Harry Poggers <mfa.g4e@gmail.com>
2024-08-12 20:33:28 +0800

    Made minor changes in title declaration

--- app.py
+++ app.py
@@ -17,19 +17,24 @@ def get_index():
 @app.route('/recommend', methods=['GET'])
 def recommend():
     title = request.args.get('title')
-    if title not in df.index:
+    title = "I'll be Seeing You".strip().lower().replace("'", '')
+
+    # Find the index of the matching title (if it exists)
+    matching_index = df.index.str.strip().str.lower().str.replace("'", '').get_loc(title)
+
+    if not pd.isnull(matching_index):
+        title = df.index[matching_index]
+        book_vector = df.loc[title].values.reshape(1, -1)
+        distances, indices = model.kneighbors(book_vector, n_neighbors=6)
+
+        recommended_books = pd.DataFrame({
+            'title': df.index[indices.flatten()][1:],  
+            'distance': distances.flatten()[1:]       
+        })
+        return jsonify(recommended_books.to_dict(orient='records'))
+    
+    else:
         return jsonify({'error': f"Book '{title}' not found"}), 404
-
-    book_vector = df.loc[title].values.reshape(1, -1)
-
-    distances, indices = model.kneighbors(book_vector, n_neighbors=6)
-
-    recommended_books = pd.DataFrame({
-        'title': df.index[indices.flatten()][1:],  
-        'distance': distances.flatten()[1:]       
-    })
-
-    return jsonify(recommended_books.to_dict(orient='records'))
 
 if __name__ == '__main__':
     app.run(debug=True)
