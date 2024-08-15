b08959855c0b71d29ae3febc98eb8471ccfdcc2c
Harry Poggers <mfa.g4e@gmail.com>
2024-08-15 14:45:35 +0800

    Used a different approach

--- app.py
+++ app.py
@@ -1,66 +1,24 @@
-import joblib
-import pandas as pd
+from book import Book
 from flask import Flask, jsonify, request
-import re
 
-model = joblib.load('book_recommender_model.joblib')
-
-df = pd.read_csv('preprocessed_book_data.csv')  
-books_df = pd.read_csv('book\Books.csv')
-
-df.set_index('Book-Title', inplace=True)  
+book = Book(
+    'book/filtered/Books.csv',
+    'preprocessed.csv',
+    'model.joblib'
+)
 
 app = Flask(__name__)
 
-@app.route('/get_index', methods=['GET'])
-def get_index():
-    return jsonify(df.index.tolist())
-
-def normalize_title(title):
-    """Normalize the book title by stripping spaces, lowercasing, and removing special characters."""
-    return re.sub(r'\W+', '', str(title).strip().lower())
-
 @app.route('/recommend', methods=['GET'])
 def recommend():
-    title = request.args.get('title').strip().lower().replace("'", '')
+    title = request.args.get('title')
 
-    normalized_title = normalize_title(title)
+    recommendations = book.getRecommendations(title)[1]
 
-    try:
-        normalized_index = df.index.to_series().apply(normalize_title)
-        matching_index = normalized_index.index[normalized_index == normalized_title][0]
-    except IndexError:
-        return jsonify({'error': f"Book '{title}' not found"}), 404
-
-    recommended_titles = []
-
-    if not pd.isnull(matching_index):
-        title = df.index[matching_index]
-        book_vector = df.loc[title].values.reshape(1, -1)
-        distances, indices = model.kneighbors(book_vector, n_neighbors=6)
-
-        recommended_titles = df.index[indices.flatten()][1:].tolist()
-
-        books_info = []
-        for rec_title in recommended_titles:
-
-            normalized_rec_title = normalize_title(rec_title)
-
-            try:
-                book_info = books_df.loc[books_df.index.to_series().apply(normalize_title) == normalized_rec_title]
-                book_info = book_info.iloc[0]  
-                books_info.append({
-                    'title': rec_title,
-                    'author': book_info['Book-Author'],
-                    'year': book_info['Year-Of-Publication'],
-                    'publisher': book_info['Publisher']
-                })
-            except IndexError:
-                print(f"Book '{rec_title}' not found in the books DataFrame.")
-
-        return jsonify({'recommended_titles': recommended_titles, 'books_info': books_info})
+    if len(recommendations) == 0:
+        return jsonify({'error': f"Book ' {title} ' not found"}), 404
     else:
-        return jsonify({'error': f"Book '{title}' not found"}), 404
+         return jsonify(recommendations)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ book.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import joblib
+
+class Book:
+    def __init__(self, dir_book: str, dir_preprocessed: str, dir_model: str):
+        self.df_book = self.getDF(dir_book, index='Book-Title')
+        self.df_preprocessed = self.getDF(dir_preprocessed, index='Book-Title')
+
+        self.model = joblib.load(dir_model)
+
+    def getDF(self,dir: str, index=''):
+        df = pd.read_csv(dir)
+        if index != '': 
+            df = df.set_index(index)
+        return df
+
+    def getRecommendations(self, title: str):
+        if title not in self.df_preprocessed.index:
+            print(f"The book '{title}' does not exist in the dataset.")
+            return [title, []]
+
+        book_vector = self.df_preprocessed.loc[title].values.reshape(1, -1)
+
+        distances, indices = self.model.kneighbors(book_vector, n_neighbors=6)
+        distances, indices = distances.flatten()[1:], indices.flatten()[1:]
+
+        df_recommended = pd.DataFrame({
+            'title': self.df_preprocessed.index[indices],
+            'distance': distances
+        }).set_index('title')
+
+        df_recommended = df_recommended.join(self.df_book, how='inner').reset_index().rename(columns={
+            "Book-Author": "author",
+            "ISBN": "isbn",
+            "Image-URL-L": "img-l",
+            "Image-URL-M": "img-m",
+            "Image-URL-S": "img-s",
+            "Publisher": "publisher",
+            "Year-Of-Publication": "year" 
+        })
+        recommended = df_recommended.sort_values(by='distance', ascending=True).to_dict(orient='records')
+
+        return [title, recommended]
